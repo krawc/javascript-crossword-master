84e910c8da3a93a30005c5ef634b6fcc6e4ccfd6
krawc <konradkrawczyk@Konrads-MacBook-Air.local>
2016-09-25 23:10:41 +0800

    idk i just wanna upload it
    ttttttt

---
+++ puzzle.js
@@ -0,0 +1,100 @@
+{
+  "title": "Sample Puzzle",
+  "by": "By Line",
+  "clues": [
+    { "d":"A", "n":1, "x":0, "y":0, "a":"LEOTOLSTOY", "c":"“Happy families are all alike; every unhappy family is unhappy in its own way”, is this Russian author’s most famous opening line" },
+    { "d":"A", "n":9, "x":11, "y":0, "a":"ALTJ", "c":"Δ is this keyboard-key band’s favorite shape" },
+    { "d":"A", "n":13, "x":0, "y":1, "a":"ABRA", "c":"Pokémon, or half of a magic trick" },
+    { "d":"A", "n":14, "x":5, "y":1, "a":"OE", "c":"__dipus Rex" },
+    { "d":"A", "n":15, "x":8, "y":1, "a":"MONDIEU", "c":"My Lord in Paris" },
+    { "d":"A", "n":17, "x":1, "y":2, "a":"RANGING", "c":"The Great _____, where the Night’s Watch lost the majority of its men" },
+   
+    { "d":"A", "n":19, "x":9, "y":2, "a":"GOUMEN", "c":"Chinese doggie door, or a homophone for a chauvinist’s chant" },
+  
+  { "d":"A", "n":20, "x":0, "y":3, "a":"TINHOSE", "c":" Underwear for Dorothy’s heartless companion" },
+    
+    { "d":"A", "n":21, "x":8, "y":3, "a":"AATLONI", "c":"1 1 20 12 15 14 9" },
+    
+    { "d":"A", "n":22, "x":0, "y":4, "a":"WEG", "c":"Way for the Germans and the Dutch" },
+   
+      { "d":"A", "n":23, "x":6, "y":4, "a":"CARLOT", "c":"Auto yard" },
+ 
+    { "d":"A", "n":24, "x":13, "y":4, "a":"SP", "c":"Spanish or Spelling?" },
+    
+    { "d":"A", "n":25, "x":0, "y":5, "a":"IFEDORA", "c":"Apple hats for Sinatra or dorks" },
+    
+    { "d":"A", "n":28, "x":8, "y":5, "a":"TER", "c":"3 times your Total Expense Ratio" },
+    { "d":"A", "n":29, "x":12, "y":5, "a":"LYE", "c":"Caustic falsehood?" },
+    { "d":"A", "n":30, "x":2, "y":6, "a":"JILU", "c":"To record this dialect of Mandarin, one should travel to Hebei or Shandong" },
+
+
+
+    { "d":"A", "n":32, "x":11, "y":6, "a":"IA", "c":"Among other things in Latin (Abbr.)" },
+    
+    { "d":"A", "n":33, "x":0, "y":7, "a":"THUG", "c":"Young ____, Slime Season artist" },
+    { "d":"A", "n":34, "x":5, "y":7, "a":"MAIMONIDES", "c":"12th century philosopher astronomer, acronymed Rambam" },
+    { "d":"A", "n":38, "x":2, "y":8, "a":"LIPO", "c":"Suck for this tummy tuck" },
+    { "d":"A", "n":40, "x":7, "y":8, "a":"NEVERDIE", "c":"Supposedly a hero won’t, but in reality nobody?" },
+    
+
+
+
+
+    { "d":"A", "n":41, "x":0, "y":9, "a":"WHITERHINO", "c":"Poached and nearly extinct, this safari animal is named after its colour, and most distinctive feature" },
+    { "d":"A", "n":44, "x":11, "y":9, "a":"CEDE", "c":"To surrender" },
+    { "d":"A", "n":45, "x":0, "y":10, "a":"NOUS", "c":"What we is in French, what we hope to have in English?" },
+    { "d":"A", "n":46, "x":5, "y":10, "a":"HOPS", "c":"Flowers used in creation of beer" },
+    { "d":"A", "n":48, "x":0, "y":11, "a":"EMS", "c":"Emergency Medical Services" },
+    
+
+
+    { "d":"A", "n":49, "x":4, "y":11, "a":"DAMOCLES", "c":"There was a sharpened sword above his head / That hung there by the thinnest simple thread” - Chaucer" },
+    { "d":"A", "n":53, "x":0, "y":12, "a":"RE", "c":"To surrender" },
+    { "d":"A", "n":55, "x":5, "y":12, "a":"SOPHOMORES", "c":"Class of 2019" },
+    { "d":"A", "n":58, "x":1, "y":13, "a":"REALISE", "c":"Flowers used in creation of beer" },
+    { "d":"A", "n":61, "x":9, "y":13, "a":"VOIDED", "c":"Made null" },
+    { "d":"A", "n":62, "x":0, "y":14, "a":"ASSENT", "c":"Agreement, or a very rude slow-talking tree" },
+    { "d":"A", "n":63, "x":8, "y":14, "a":"MENTALS", "c":"Asylum residents, or cheeky lads" },
+
+    { "d":"D", "n":1, "x":0, "y":0, "a":"LA", "c":"Please baby no more parties in __, Kanye raps while eating 麻__烫" },
+    { "d":"D", "n":2, "x":1, "y":0, "a":"EBRIEF", "c":"Online version of a legal write-up" },
+    { "d":"D", "n":3, "x":2, "y":0, "a":"ORANGEJULIUS", "c":"Zoolander's crew pre-gasoline fight drinks, or a tanned Caesar" },
+    { "d":"D", "n":4, "x":3, "y":0, "a":"TANH", "c":"Hyperbolic tangent function" },
+    { "d":"D", "n":5, "x":5, "y":0, "a":"LOIS", "c":"Superman's girlfriend ___ Lane, or pious New Testament game" },
+    { "d":"D", "n":6, "x":6, "y":0, "a":"SENECA", "c":"“Drunkenness is nothing but voluntary madness”, namesake of the 72nd-74th Hunger Games’ showrunner writes in a Stoic letter" },
+    { "d":"D", "n":7, "x":8, "y":0, "a":"OM", "c":"Mystic syllable (at the beginning and end of most Sanskrit recitations, prayers, and texts), __ Shanti __" },
+    { "d":"D", "n":8, "x":9, "y":0, "a":"YOGALENOVO", "c":"Limber Chinese computer?" },
+    { "d":"D", "n":9, "x":11, "y":0, "a":"ADULT", "c":"A fully developed human, probably not you (yet)" },
+    { "d":"D", "n":10, "x":12, "y":0, "a":"LIMO", "c":"Take a stretch car from Long Island to Missouri" },
+    { "d":"D", "n":11, "x":13, "y":0, "a":"TEENSY", "c":"Small, with an angsty start" },
+    { "d":"D", "n":12, "x":14, "y":0, "a":"JUNIPERSEED", "c":"Gin flavoring gem" },
+    { "d":"D", "n":16, "x":10, "y":1, "a":"NOTOR", "c":"___ious BIG" },
+    { "d":"D", "n":18, "x":4, "y":2, "a":"GOGOL", "c":"Russian author of Dead Souls" },
+    { "d":"D", "n":20, "x":0, "y":3, "a":"TWINTOWNER", "c":"Half a pair of identical sibling city-dwellers" },
+    { "d":"D", "n":21, "x":8, "y":3, "a":"ART", "c":"Always subjective imitation of nature partner of Simon" },
+    { "d":"D", "n":26, "x":3, "y":5, "a":"DIGITS", "c":"Outdated hipster would ask for these, when attempting to hit" },
+    { "d":"D", "n":27, "x":5, "y":5, "a":"RUMORHASIT", "c":"Underrated Adele song and 2005 Rob Reiner rom-com" },
+    { "d":"D", "n":29, "x":12, "y":5, "a":"LADDER", "c":"Climb the chute to meet a snake at 90 degrees" },
+    { "d":"D", "n":31, "x":7, "y":6, "a":"MINIPOPE", "c":"Tiny Francis - Our Lord on Earth?" },
+    { "d":"D", "n":32, "x":11, "y":6, "a":"IIRC", "c":"If I recall correctly (Abbr.)" },
+    { "d":"D", "n":35, "x":8, "y":7, "a":"MENSCH", "c":"What a person of integrity would be called by 34A" },
+   
+
+    { "d":"D", "n":36, "x":10, "y":7, "a":"NE", "c":"North East (Abbr.)" },
+    { "d":"D", "n":37, "x":13, "y":7, "a":"EID", "c":"Muslim holiday, homophone for the past tense of “ee”" },
+    { "d":"D", "n":39, "x":4, "y":8, "a":"PE", "c":"Physical Education (Abbr.)" },
+    { "d":"D", "n":42, "x":1, "y":9, "a":"HOMERS", "c":"If Simpson and the author of the Iliad and the Odyssey would come together, one could observe this" },
+   
+    { "d":"D", "n":43, "x":6, "y":9, "a":"HOMOS", "c":"Many a not-hetero Sapien" },
+    { "d":"D", "n":47, "x":10, "y":10, "a":"LEMON", "c":"Fruit needed to make a Beyonce album" },
+    { "d":"D", "n":50, "x":9, "y":11, "a":"LOVE", "c":"This temporary insanity was only curable by marriage, according to Ambrose Bierce, forever according to Billy Ocean, while the Smiths claim it will bring us together" },
+    { "d":"D", "n":51, "x":11, "y":11, "a":"SOIT", "c":"So (be) it in French" },
+
+    { "d":"D", "n":52, "x":13, "y":11, "a":"HEEL", "c":"Achilles’ weak spot" },
+    { "d":"D", "n":54, "x":3, "y":12, "a":"BAE", "c":"British Aerospace’s slangy love interest" },
+    { "d":"D", "n":56, "x":12, "y":12, "a":"RDA", "c":"Recommended Daily Allowance (Abbr.)" },
+    { "d":"D", "n":57, "x":14, "y":12, "a":"SDS", "c":"Students for a Democratic Society (Abbr.)" },
+    { "d":"D", "n":59, "x":2, "y":13, "a":"ES", "c":"Is and the top-level domain in Spain" },
+    { "d":"D", "n":60, "x":4, "y":13, "a":"LN", "c":"Natural logarithm" }
+  ]
+}
